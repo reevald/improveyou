23c4709dd4fbf5b276049ab9b29bc43d248658a0
reevald <mochammadgalangrivaldo@gmail.com>
2023-08-06 20:26:50 +0700

    Fix logic login, add user change username and public mode serializer

--- api/app/users/serializers.py
+++ api/app/users/serializers.py
@@ -1,6 +1,10 @@
 import django.contrib.auth.password_validation as validators
+from django.conf import settings
 from django.core import exceptions
+from django.core.validators import EmailValidator
+from django.utils import timezone
 from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
 
 from .models import GameStat, Object, User
 
@@ -43,20 +47,39 @@ class RegisterUserSerializer(serializers.ModelSerializer):
 class LoginUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id"]
+        fields = ["id", "email", "password"]
+        extra_kwargs = {
+            "email": {"validators": [EmailValidator]},
+            "password": {"required": True},
+        }
+
+    def validate(self, data):
+        email = data.get("email")
+        password = data.get("password")
+        try:
+            user = self.Meta.model.objects.get(email=email)
+        except self.Meta.model.DoesNotExist:
+            raise AuthenticationFailed(
+                "Sorry, that email doesn't exist. Please try again."
+            )
+        if not user.check_password(password):
+            raise AuthenticationFailed(
+                "Sorry, that password isn't right. Please try again."
+            )
+        data["user"] = user  # For credential jwt & update last login
+        return data
 
 
 class UserObjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Object
-        fields = ["user_id", "character_id", "hat_id", "clothes_id", "shoes_id"]
+        fields = ["character_id", "hat_id", "clothes_id", "shoes_id"]
 
 
 class UserGameStatSerializer(serializers.ModelSerializer):
     class Meta:
         model = GameStat
         fields = [
-            "user_id",
             "gold",
             "poin_brain",
             "poin_heart",
@@ -64,3 +87,47 @@ class UserGameStatSerializer(serializers.ModelSerializer):
             "streak_current",
             "streak_percent_interest",
         ]
+
+
+class UserChangeUsernameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "last_username_changed"]
+        extra_kwargs = {"username": {"required": True}}
+
+    def update(self, instance, validated_data):
+        if instance.username == validated_data.get("username"):
+            raise serializers.ValidationError(
+                {"username": ["This field must be not same with currently in use"]}
+            )
+        now = timezone.now()
+        if now - instance.last_username_changed < settings.USERNAME_CHANGE_PERIOD:
+            raise serializers.ValidationError(
+                "You can't change username right now because you've changed it "
+                f"once within {settings.USERNAME_CHANGE_PERIOD.days} days"
+            )
+
+        instance.username = validated_data.get("username")
+        instance.last_username_changed = now
+        instance.save()
+        return instance
+
+
+class UserChangePublicitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["is_public_mode"]
+        extra_kwargs = {"is_public_mode": {"required": True}}
+
+    def update(self, instance, validated_data):
+        if instance.is_public_mode == validated_data.get("is_public_mode"):
+            raise serializers.ValidationError(
+                {
+                    "is_public_mode": [
+                        "This field must be not same with currently in use"
+                    ]
+                }
+            )
+        instance.is_public_mode = validated_data.get("is_public_mode")
+        instance.save()
+        return instance
