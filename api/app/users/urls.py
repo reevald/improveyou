0503e18d3e39b3e8b53718a359b3a6d69fbdeebe
reevald <mochammadgalangrivaldo@gmail.com>
2023-08-06 20:29:29 +0700

    Add endpoint for change username and public mode

--- api/app/users/urls.py
+++ api/app/users/urls.py
@@ -9,5 +9,15 @@ urlpatterns = [
     path("refresh", views.RefreshAccessTokenView.as_view(), name="refresh-token"),
     path("me/object", views.UserMeObjectView.as_view(), name="user-object"),
     path("me/gamestat", views.UserMeGameStatView.as_view(), name="user-gamestat"),
+    path(
+        "me/username",
+        views.UserMeChangeUsernameView.as_view(),
+        name="user-change-username",
+    ),
+    path(
+        "me/publicity",
+        views.UserMeChangePublicityView.as_view(),
+        name="user-change-publicity",
+    ),
     path("me/tracker", views.UserMeTrackerView.as_view(), name="user-tracker"),
 ]
