fb8fbb559bb0b38cbce891d77bd4afffd6cd1900
reevald <mochammadgalangrivaldo@gmail.com>
2023-08-07 23:51:17 +0700

    Add buy item view

--- api/app/items/views.py
+++ api/app/items/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from users.auths import JWTAuthentication
 
 from .models import Item
-from .serializers import ItemShopSerializer
+from .serializers import BuyItemSerializer, ItemShopSerializer
 
 
 class ShopItemView(APIView):
@@ -15,3 +15,15 @@ class ShopItemView(APIView):
         shop_items = Item.objects.filter(availability="shop")
         serializer = ItemShopSerializer(shop_items, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class BuyItemView(APIView):
+    throttle_scope = "buy_item"
+    authentication_classes = [JWTAuthentication]
+
+    def put(self, request):
+        serializer = BuyItemSerializer(self.request.user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
