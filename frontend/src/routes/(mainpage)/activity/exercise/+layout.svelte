478d0458da6f5c5f1766ea862ca64424be6299fb
reevald <mochammadgalangrivaldo@gmail.com>
2023-09-04 12:15:11 +0700

    MVP detail exercise activity

--- frontend/src/routes/(mainpage)/activity/exercise/+layout.svelte
+++
@@ -1,3 +0,0 @@
-<div class="bg-red-900">[Begin] activity exercise layout</div>
-<slot />
-<div class="bg-red-900">[End] activity exercise layout</div>
--- frontend/src/routes/(mainpage)/activity/exercise/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h1>Activity exercise page</h1>
