00b40ceff69a4ad83c3d3b524fce0014a57ca12a
reevald <mochammadgalangrivaldo@gmail.com>
2023-09-04 12:21:30 +0700

    MVP login page

--- frontend/src/routes/(account)/login/+page.server.js
+++ frontend/src/routes/(account)/login/+page.server.js
@@ -1,45 +1,31 @@
 export const ssr = true;
+// console.log('[page server login] Its run server?');
+import { iyPOST } from '$lib/api';
+import { baseAPI, cookieDev } from '$lib/const';
 import { cookieNameJWT } from '$lib/stores.js';
-// const urlLogin = 'http://127.0.0.1:8000/user/login';
-// const urlLogin = 'https://api.improveyou.fun/user/login';
-// const urlRefresh = 'https://api.improveyou.fun/user/refresh';
-const testLogin = async (url = '', credType = 'same-origin', data = {}) => {
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: credType, // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response; // parses JSON response into native JavaScript objects
-};
 
 // /** @type {import('./$types').Actions} */
 export const actions = {
-  login: async ({ cookies, request }) => {
+  process: async ({ cookies, request }) => {
+    // Dev mode
+    const cookieName = cookieDev; // Prod mode: cookieProd
+
     const data = await request.formData();
     const email = data.get('email');
     const password = data.get('password');
-    const endpoint = data.get('endpoint');
-    const credType = data.get('credType');
-    const cookieName = data.get('cookieName');
-    const response = await testLogin(endpoint, credType, {
+    const response = await iyPOST(baseAPI + 'user/login', 'include', {
       email,
       password
     });
-    cookies.set('test cookie1', 'value cookie1');
-    const cookieku = response.headers.get('set-cookie');
-    if (cookieku) {
-      const values = cookieku.split(';').reduce((res, item) => {
+    const iyCookie = response.headers.get('set-cookie');
+    if (iyCookie) {
+      const values = iyCookie.split(';').reduce((res, item) => {
         const data = item.trim().split('=');
         return { ...res, [data[0]]: data[1] };
       }, {});
       cookieNameJWT.update(() => cookieName);
+
+      // Prod Mode
       cookies.set(cookieName, values[cookieName], {
         expires: new Date(values['expires']),
         maxAge: values['Max-Age'],
@@ -49,8 +35,21 @@ export const actions = {
         secure: true,
         domain: 'improveyou.fun'
       });
-      // return { success: true };
+
+      // Dev Mode
+      // cookies.set(cookieName, values[cookieName], {
+      //   expires: new Date(values['expires']),
+      //   maxAge: values['Max-Age'],
+      //   path: '/',
+      //   httpOnly: true,
+      //   sameSite: 'lax', // true = Strict
+      //   secure: false,
+      //   domain: 'localimproveyou.fun' // with custom hosts
+      // });
     }
-    return await response.json();
+    return {
+      status: response.status,
+      data: await response.json()
+    };
   }
 };
--- frontend/src/routes/(account)/login/+page.svelte
+++ frontend/src/routes/(account)/login/+page.svelte
@@ -1,34 +1,109 @@
 <script>
+  import { goto } from '$app/navigation';
+  import { tryAccessWithAuth } from '$lib/api.js';
+  import { onMount } from 'svelte';
+
+  // console.log('[page login] Its run server?');
   export let form;
-  if (typeof localStorage !== 'undefined' && form) {
-    localStorage.setItem('jwt', form?.jwt);
-  }
+
+  onMount(() => {
+    if (form) {
+      if (typeof localStorage !== 'undefined' && form?.data?.jwt) {
+        localStorage.setItem('jwt', form.data.jwt);
+      }
+      if (form?.status == 200) {
+        goto('home');
+      } else if (form?.status == 429) {
+        const detail = form?.data?.detail;
+        goto(`429-too-many-requests?sec=${detail.match(/\d+/)}`);
+      } else {
+        const msgElem = document.getElementById('messageBox');
+        msgElem.style.display = 'flex';
+        msgElem.children[1].innerText = form?.data?.detail;
+      }
+    } else {
+      tryAccessWithAuth('GOIN');
+    }
+
+    const inputElem = document.querySelectorAll('.input-group input');
+    const getLabelElem = (id) => document.querySelector(`label[for='${id}']`);
+    if (inputElem) {
+      inputElem.forEach((elem) => {
+        elem.addEventListener('focus', (event) => {
+          getLabelElem(event.target.getAttribute('id')).classList.add('animate-label');
+        });
+        elem.addEventListener('blur', (event) => {
+          if (event.target.value == '') {
+            getLabelElem(event.target.getAttribute('id')).classList.remove(
+              'animate-label'
+            );
+          }
+        });
+      });
+    }
+  });
 </script>
 
-<a href="/logout">Logout</a>
-<a href="/home">Home</a>
-<h1>Login page attempt 1</h1>
-
-<form method="POST" action="?/login">
-  <label>
-    Url endpoint
-    <input name="endpoint" type="text" />
-  </label>
-  <label>
-    Cookie name
-    <input name="cookieName" type="text" />
-  </label>
-  <label>
-    Credential type
-    <input name="credType" type="text" />
-  </label>
-  <label>
-    Email
-    <input name="email" type="email" />
-  </label>
-  <label>
-    Password
-    <input name="password" type="password" />
-  </label>
-  <button>Log in</button>
-</form>
+<div
+  class="bg-sky-950 flex flex-col justify-center w-full xs:w-96 rounded-md xs:shadow-gray-950/80 shadow-2xl z-20 max-h-screen h-full xs:h-auto"
+>
+  <div class="flex flex-row justify-center mb-6 px-8 mt-8 z-20">
+    <img src="/img/logo.png" class="w-auto xs:w-56" alt="" />
+  </div>
+  <form class="px-8 mb-8 z-30 overflow-y-auto" method="POST" action="?/process">
+    <div
+      id="messageBox"
+      class="bg-red-200 hidden p-2 mb-4 flex-row items-start rounded-md"
+    >
+      <div class="w-10 mr-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+          ><path
+            d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 15H13V17H11V15ZM11 7H13V13H11V7Z"
+            fill="rgba(239,68,68,1)"
+          /></svg
+        >
+      </div>
+      <div class="text-sm w-full"></div>
+    </div>
+    <div class="input-group group mb-4 mt-2">
+      <label
+        for="email"
+        class="bg-sky-950 z-30 m-3 px-1 text-gray-400 cursor-text group-focus-within:cursor-default pointer-events-none"
+        >Email</label
+      >
+      <input
+        class="border-2 bg-sky-950 autofill:shadow-[inset_0_0_0px_1000px_rgb(8,47,73)] autofill:transition-colors autofill:duration-[5000000ms] border-sky-900 p-2.5 text-gray-300 focus:border-sky-400 outline-none rounded-md"
+        type="email"
+        name="email"
+        id="email"
+        required
+      />
+    </div>
+    <div class="input-group group mb-4">
+      <label
+        for="password"
+        class="bg-sky-950 m-3 px-1 text-gray-400 group-focus-within:cursor-default pointer-events-none"
+        >Password</label
+      >
+      <input
+        class="border-2 bg-sky-950 border-sky-900 p-2.5 text-gray-300 focus:border-sky-400 outline-none rounded-md"
+        type="password"
+        name="password"
+        id="password"
+        required
+      />
+    </div>
+    <button
+      type="submit"
+      class="bg-amber-500 hover:bg-amber-600 rounded-md text-lg font-bold flex flex-row items-center justify-center w-full h-12 mb-4"
+    >
+      Login
+    </button>
+    <div class="text-gray-400">
+      Not a member yet? <a
+        href="signup"
+        class="text-amber-500 hover:text-amber-600 font-semibold">Sign Up</a
+      >
+    </div>
+  </form>
+</div>
