df6282be63ccf679989d6957a069c2b0d8f38b61
reevald <mochammadgalangrivaldo@gmail.com>
2023-09-04 13:01:15 +0700

    Fix baseAPI url

--- frontend/src/lib/const.js
+++ frontend/src/lib/const.js
@@ -2,5 +2,5 @@
 // export const cookieProd = '__Secure-refresh-jwt';
 // export const cookieDev = '__Dev-refresh-jwt';
 
-export const baseAPI = 'http://api.improveyou.fun/';
+export const baseAPI = 'https://api.improveyou.fun/';
 export const cookieDev = '__Secure-refresh-jwt';
